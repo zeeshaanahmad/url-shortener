cffbc94122ebf3465b50961ee22e8ec61b85993f
Zeeshan <ahmad.zeeshaan@gmail.com>
2020-02-06 01:11:32 +0300

    update: added javadoc to ShorteningUtil

--- src/main/java/com/neueda/interview/urlshortener/common/ShorteningUtil.java
+++ src/main/java/com/neueda/interview/urlshortener/common/ShorteningUtil.java
@@ -1,9 +1,19 @@
 package com.neueda.interview.urlshortener.common;
 
+/**
+ * Provides encoding and decoding methods Base10 to Base62 and vice versa
+ */
 public class ShorteningUtil {
+    /**
+     * Randomized a-z A-Z 0-9 characters. Each character appears only once in the string
+     */
     public static final String ALPHABET = "Mheo9PI2qNs5Zpf80TBn7lmRbtQ4YKXHvwAEWxuzdra316OJigGLSVUCyFjkDc";
     public static final int BASE = ALPHABET.length();
 
+    /**
+     * @param num Base10 number of type Long
+     * @return base62 encoded string representation of input Long
+     */
     public static String idToStr(Long num) {
         StringBuilder str = new StringBuilder();
         while (num > 0) {
@@ -13,6 +23,10 @@ public class ShorteningUtil {
         return str.toString();
     }
 
+    /**
+     * @param str Base62 encoded string
+     * @return decoded Base10 number of type Long
+     */
     public static Long strToId(String str) {
         Long num = 0L;
         for (int i = 0; i < str.length(); i++) {
