1a646889938313e28bb6ee315868754f1565f3a2
Zeeshan <ahmad.zeeshaan@gmail.com>
2020-02-06 18:44:37 +0300

    update: Logging implemented on UrlService

--- src/main/java/com/neueda/interview/urlshortener/service/UrlService.java
+++ src/main/java/com/neueda/interview/urlshortener/service/UrlService.java
@@ -5,11 +5,15 @@ import com.neueda.interview.urlshortener.common.ShorteningUtil;
 import com.neueda.interview.urlshortener.model.UrlEntity;
 import com.neueda.interview.urlshortener.dto.FullUrl;
 import com.neueda.interview.urlshortener.dto.ShortUrl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UrlService {
+
+    Logger logger = LoggerFactory.getLogger(UrlService.class);
 
     private final UrlRepository urlRepository;
 
@@ -19,6 +23,7 @@ public class UrlService {
     }
 
     private UrlEntity get(Long id) {
+        logger.info(String.format("Fetching Url from database for Id %d", id));
         UrlEntity urlEntity = urlRepository.findById(id).get();
         return urlEntity;
     }
@@ -30,7 +35,12 @@ public class UrlService {
      * @return FullUrl object
      */
     public FullUrl getFullUrl(String shortenString) {
-        return new FullUrl(this.get(ShorteningUtil.strToId(shortenString)).getFullUrl());
+        logger.debug("Converting Base 62 string %s to Base 10 id");
+        Long id = ShorteningUtil.strToId(shortenString);
+        logger.info(String.format("Converted Base 62 string %s to Base 10 id %s", shortenString, id));
+
+        logger.info(String.format("Retrieving full url for %d", id));
+        return new FullUrl(this.get(id).getFullUrl());
     }
 
     private UrlEntity save(FullUrl fullUrl) {
@@ -44,8 +54,14 @@ public class UrlService {
      * @return ShortUrl object
      */
     public ShortUrl getShortUrl(FullUrl fullUrl) {
+        logger.info(String.format("Saving Url %s to database", fullUrl.getFullUrl()));
         UrlEntity savedUrl = this.save(fullUrl);
+        logger.debug(savedUrl.toString());
+
+        logger.debug(String.format("Converting Base 10 %d to Base 62 string", savedUrl.getId()));
         String shortUrlText = ShorteningUtil.idToStr(savedUrl.getId());
+        logger.info(String.format("Converted Base 10 %d to Base 62 string %s", savedUrl.getId(), shortUrlText));
+
         return new ShortUrl(shortUrlText);
     }
 }
