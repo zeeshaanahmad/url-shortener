a00bc5f28e0d233087038f454f7a094a98aac42d
Zeeshan <ahmad.zeeshaan@gmail.com>
2020-02-06 20:07:51 +0300

    add: method to check if full url already exists

--- src/main/java/com/neueda/interview/urlshortener/repository/UrlRepository.java
+++ src/main/java/com/neueda/interview/urlshortener/repository/UrlRepository.java
@@ -2,8 +2,14 @@ package com.neueda.interview.urlshortener.repository;
 
 import com.neueda.interview.urlshortener.model.UrlEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
+
+    @Query("SELECT u FROM url u WHERE u.fullUrl = ?1")
+    List<UrlEntity> findUrlByFullUrl(String fullUrl);
 }
